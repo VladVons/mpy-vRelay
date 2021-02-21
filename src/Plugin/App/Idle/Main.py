aeefde96ba15b52747896de608212fc43d5516b0
VladVons <vladvons@gmail.com>
2021-02-21 19:08:52 +0200

    MyCommit

--- src/Plugin/App/Idle/Main.py
+++ src/Plugin/App/Idle/Main.py
@@ -30,6 +30,7 @@ class TIdle():
         #        print('mqtt public')
         #        mqtt.Publish('MyTopic1', 'tLedBeat %s' % self.CntLoop)
 
+    '''
     def tConfClear(self):
         O = Pin(0, Pin.IN, Pin.PULL_UP)
         if (not O.value()):
@@ -42,41 +43,55 @@ class TIdle():
                 Reset()
         else:
             self.CntBtn = 0 
+    '''
 
+    '''
     def tDSleep(self):
         if (Conf.DSleep) and (self.CntLoop % 60 == 0):
             Reset(Conf.get('DSleep', 1*60))
+    '''
 
+    '''
     def tMemFree(self):
         gc.collect()
         print('mem_free', gc.mem_free())
 
         Net = WLAN(STA_IF)
         print('ifconfig', Net.ifconfig()[0])
+    '''
 
-    #async def tWatchConnect(self):
-    #    Net = WLAN(STA_IF)
-    #    if (not Net.isconnected):
-    #        WLan = TWLanApp()
-    #        await WLan.TryConnect()
+    '''
+    async def tWatchConnect(self):
+        Net = WLAN(STA_IF)
+        if (not Net.isconnected):
+            WLan = TWLanApp()
+            await WLan.TryConnect()
+    '''
 
-    #async def tWatchHost(self):
-    #    Cnt = Conf.get('WatchHost_Cnt', 0)
-    #    if (Cnt > 0) and (self.CntLoop % Cnt == 0):
-    #        WLan = TWLanApp()
-    #        await WLan.CheckConnect()
+    '''
+    async def tWatchHost(self):
+        Cnt = Conf.get('WatchHost_Cnt', 0)
+        if (Cnt > 0) and (self.CntLoop % Cnt == 0):
+            WLan = TWLanApp()
+            await WLan.CheckConnect()
+    '''
 
-    #async def DoExit(self):
-        #WDog.Stop()
-        #from Inc.Util import UHrd
-        #await UHrd.LedFlash(2, 3, 0.2)
+    '''
+    async def DoExit(self):
+        WDog.Stop()
+        from Inc.Util import UHrd
+        await UHrd.LedFlash(2, 3, 0.2)
+    '''
 
-    #async def DoPost(self, aOwner: TTask, aMsg):
-    #    print('InIdle', aOwner.Alias, aMsg)
+    '''
+    async def DoPost(self, aOwner: TTask, aMsg):
+        print('InIdle', aOwner.Alias, aMsg)
 
-    #async def _DoPost(self, aOwner, aMsg):
-    #    print('Im Idle', aOwner, aMsg)
-    #    return 'from Idle'
+    async def _DoPost(self, aOwner, aMsg):
+        print('Im Idle', aOwner, aMsg)
+        return 'from Idle'
+
+    '''
 
     async def Run(self, aSleep: float = 2):
         while True:
@@ -85,13 +100,15 @@ class TIdle():
             #self.tDSleep()
             #self.tMemFree()
             self.tLedBeat()
-            self.tConfClear()
+            #self.tConfClear()
 
             self.CntLoop += 1
             await asyncio.sleep(aSleep)
 
-            #if (self.CntLoop % 3 == 0):
-                #await Plugin.Post(self, 'from Idle')
-                #Obj = Plugin.Get('App/Mqtt')
-                #if (Obj):
-                #    Obj.Publish('aTopic1', self.CntLoop)
+            '''
+            if (self.CntLoop % 3 == 0):
+                await Plugin.Post(self, 'from Idle')
+                Obj = Plugin.Get('App/Mqtt')
+                if (Obj):
+                    Obj.Publish('aTopic1', self.CntLoop)
+            '''
