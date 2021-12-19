dd0ddfd22e7fd4ed6e87b56d60edb9a5796f4393
VladVons <vladvons@gmail.com>
2021-12-19 10:30:34 +0200

    MyCommit

--- test/mqtt/m_pub.sh
+++ test/mqtt/m_pub.sh
@@ -3,29 +3,56 @@
 #http://www.steves-internet-guide.com/mosquitto_pub-sub-clients/
 #apt install mosquitto-clients
 
-cHost="vpn2.oster.com.ua"
-cTopic="vRelay/sub/Url"
-cMsg="Sys_print.py?text=hello world"
-#cMsg="Sen_ds18b20.py?pin=14"
-
-#cTopic="vRelay/sub/Plugin.App.Therm"
-#cMsg="sys_print.py?text=hello world"
+#cHost="vpn2.oster.com.ua"
+cHost="192.168.2.115"
 
 
-Pub()
+
+Pub1()
 {
+  local Topic="vRelay/sub/Url"
+  local Msg="Sys_print.py?text=hello world"
+  #local Msg="Sen_ds18b20.py?pin=14"
+
+  #local Topic="vRelay/sub/Plugin.App.Therm"
+  #local Msg="sys_print.py?text=hello world"
+
   Cnt=0
   while true; do
     ((Cnt++))
-    Msg="$cMsg&debug=$Cnt"
-    #Msg="$cMsg"
-    echo $Msg
-    #mosquitto_pub -h $cHost -t $cTopic -m "$Msg" -d
-    mosquitto_pub -h $cHost -t $cTopic -m "$Msg"
+    Data="$Msg&debug=$Cnt"
+    #Msg="$Msg"
+
     echo
+    echo $Data
+    #mosquitto_pub -h $cHost -t $Topic -m "$Data" -d
+    mosquitto_pub -h $cHost -t $Topic -m "$Data"
 
     sleep 3
   done
 }
 
-Pub
+
+Pub2()
+{
+  local Topic="vRelay/pub/post"
+
+  Cnt=0
+  while true; do
+    ((Cnt++))
+    Date=$(date "+%Y-%m-%d %H:%M:%S")
+
+    Id="6bdfc000"
+    Val="18.4"
+    Template='{"Date": "%s", "Owner": "TTherm", "Data": {"Owner": "TSen_dht22_t", "Uptime": %s, "Val": %s}, "Id": "%s", "Alias": "Sen_dht22"}'
+    Data=$(printf "$Template" "$Date" "$Cnt" "$Val" "$Id")
+    echo $Data
+
+    mosquitto_pub -h $cHost -t $Topic -m "$Data"
+    sleep 3
+  done
+}
+
+
+#Pub1
+Pub2
--- test/temp.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-
-d1 = {'one': 1}
-print(dict(d1, **{'two': 2}))
-      
